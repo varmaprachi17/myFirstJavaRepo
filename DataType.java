4da38f10fc0ab39feb39ed30a80becfd1d36f7ea
varmaprachi17 <141847520+varmaprachi17@users.noreply.github.com>
2023-08-09 20:15:25 +0530

    Add files via upload
    secondpush

---
+++ DataType.java
@@ -0,0 +1,17 @@
+class DataType{
+public static void main(String args[]){
+int a=10;
+System.out.println(a);
+float b=10.02f;
+System.out.println(b);
+boolean c=true;
+System.out.println(c);
+double d=100.2d;
+System.out.println(d);
+char ch='a';
+System.out.println(ch);
+long l=2000;
+System.out.println(l);
+
+}
+}
